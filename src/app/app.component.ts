46b12d050c9f14f5feb3d4786c8733f7c727a2ba
Flexmonster <github@flexmonster.com>
2018-03-29 19:01:59 +0300

    Angular CLI app example

---
+++ src/app/app.component.ts
@@ -0,0 +1,53 @@
+import { Component, ViewChild } from '@angular/core';
+import { FlexmonsterPivot } from 'ng-flexmonster';
+import { Flexmonster } from 'ng-flexmonster';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  @ViewChild('pivot') pivot: FlexmonsterPivot;
+  public pivotReport = {
+    dataSource: {
+      filename: 'https://cdn.flexmonster.com/data/data.csv'
+    },
+    slice: {
+      rows: [
+        { uniqueName: 'Destination' },
+        { uniqueName: 'Color' },
+        { uniqueName: '[Measures]' }
+      ],
+      columns: [
+        { uniqueName: 'Category' },
+        { uniqueName: 'Country' }
+      ],
+      measures: [
+        { uniqueName: 'Price', aggregation: 'sum' },
+        { uniqueName: 'Quantity', aggregation: 'sum' }
+      ]
+    }
+  }
+
+  onPivotReady(pivot: Flexmonster.Pivot): void {
+    console.log("[ready] FlexmonsterPivot", this.pivot);
+  }
+
+  onCustomizeCell(cell: Flexmonster.CellBuilder, data: Flexmonster.Cell): void {
+    //console.log("[customizeCell] FlexmonsterPivot");
+    if (data.isClassicTotalRow) cell.addClass("fm-total-classic-r");
+    if (data.isGrandTotalRow) cell.addClass("fm-grand-total-r");
+    if (data.isGrandTotalColumn) cell.addClass("fm-grand-total-c");
+  }
+
+  onReportComplete(): void {
+    this.pivot.flexmonster.off("reportcomplete");
+    this.pivot.flexmonster.setReport({
+      dataSource: {
+        dataSourceType: "json",
+        filename: "https://cdn.flexmonster.com/data/data.json"
+      }
+    });
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTabsModule } from '@angular/material';
+import { FlexmonsterPivotModule } from 'ng-flexmonster';
+import { TabsComponent } from './tabs/tabs.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TabsComponent
+  ],
+  imports: [
+    BrowserModule, BrowserAnimationsModule, MatTabsModule, FlexmonsterPivotModule
+  ],
+  exports: [MatTabsModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
