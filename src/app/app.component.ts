ef463d8eb6ae205b068defad5c32dbc53bee46ed
Flexmonster Pivot Table <github@flexmonster.com>
2019-03-15 15:28:11 +0200

    ng-flexmonster to 2.7 update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import { FlexmonsterPivot } from 'ng-flexmonster';
-import { Flexmonster } from 'ng-flexmonster';
+import * as Flexmonster from 'flexmonster';
 
 @Component({
   selector: 'app-root',
@@ -28,25 +28,31 @@ export class AppComponent {
         { uniqueName: 'Quantity', aggregation: 'sum' }
       ]
     }
-  }
+  };
 
   onPivotReady(pivot: Flexmonster.Pivot): void {
-    console.log("[ready] FlexmonsterPivot", this.pivot);
+    console.log('[ready] FlexmonsterPivot', this.pivot);
   }
 
   onCustomizeCell(cell: Flexmonster.CellBuilder, data: Flexmonster.CellData): void {
-    //console.log("[customizeCell] FlexmonsterPivot");
-    if (data.isClassicTotalRow) cell.addClass("fm-total-classic-r");
-    if (data.isGrandTotalRow) cell.addClass("fm-grand-total-r");
-    if (data.isGrandTotalColumn) cell.addClass("fm-grand-total-c");
+    // console.log("[customizeCell] FlexmonsterPivot");
+    if (data.isClassicTotalRow) {
+      cell.addClass('fm-total-classic-r');
+    }
+    if (data.isGrandTotalRow) {
+      cell.addClass('fm-grand-total-r');
+    }
+    if (data.isGrandTotalColumn) {
+      cell.addClass('fm-grand-total-c');
+    }
   }
 
   onReportComplete(): void {
-    this.pivot.flexmonster.off("reportcomplete");
+    this.pivot.flexmonster.off('reportcomplete');
     this.pivot.flexmonster.setReport({
       dataSource: {
-        dataSourceType: "json",
-        filename: "https://cdn.flexmonster.com/data/data.json"
+        dataSourceType: 'json',
+        filename: 'https://cdn.flexmonster.com/data/data.json'
       }
     });
   }
