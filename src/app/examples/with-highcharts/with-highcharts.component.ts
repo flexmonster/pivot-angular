4686dbb078250523e1429bc135209c0c39619813
Masha <marvisnotamused@gmail.com>
2022-06-13 15:38:13 +0300

    Updating CSS. Updating Highcharts demo

--- src/app/examples/with-highcharts/with-highcharts.component.ts
+++ src/app/examples/with-highcharts/with-highcharts.component.ts
@@ -22,10 +22,87 @@ export class WithHighchartsComponent implements OnInit {
         toolbar.showShareReportTab = true;
     }
 
+    public report: Object = {
+        dataSource: {
+            filename: "https://cdn.flexmonster.com/data/data.csv"
+        },
+        slice: {
+            rows: [
+                {
+                    uniqueName: "Country"
+                },
+                {
+                    uniqueName: "Business Type",
+                    filter: {
+                        exclude: ["business type.[value added reseller]"]
+                    }
+                },
+                {
+                    uniqueName: "[Measures]"
+                }
+            ],
+            columns: [
+                {
+                    uniqueName: "Category",
+                    filter: {
+                        exclude: ["category.[cars]"]
+                    }
+                },
+                {
+                    uniqueName: "Color",
+                    filter: {
+                        exclude: ["color.[white]", "color.[purple]"]
+                    }
+                }
+            ],
+            measures: [
+                {
+                    uniqueName: "Price",
+                    format: "empty"
+                }
+            ],
+            expands: {
+                rows: [
+                    {
+                        tuple: ["country.[united states]"]
+                    },
+                    {
+                        tuple: ["country.[canada]"]
+                    }
+                ],
+                columns: [
+                    {
+                        tuple: ["category.[accessories]"]
+                    }
+                ]
+            }
+        },
+        conditions: [
+            {
+                formula: "#value < 5000",
+                format: {
+                    backgroundColor: "#009688",
+                    color: "#FFFFFF"
+                }
+            }
+        ],
+        formats: [
+            {
+                name: "empty",
+                currencySymbol: "$",
+                nullValue: "-"
+            }
+        ]
+    };
+
+    onReady() {
+        this.pivot.flexmonster.setReport(this.report);
+    }
+
     drawChart() {
         this.pivot.flexmonster.highcharts?.getData(
             {
-                type: "area",
+                type: "spline"
             },
             (data: Flexmonster.GetDataValueObject) => {
                 Highcharts.chart('highcharts-container', <Highcharts.Options>data);
@@ -38,6 +115,8 @@ export class WithHighchartsComponent implements OnInit {
 
     onReportComplete() {
         this.pivot.flexmonster.off("reportcomplete");
+        //this.pivot.flexmonster.drillUpCell("columns", [], "", "2018");
+        //this.pivot.flexmonster.drillUpCell("columns", [], "", "2019");
         this.drawChart();
     }
 
