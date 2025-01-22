beded586af23a0429a514f8440c3af6015c7652b
maksymkaD <52499153+maksymkaD@users.noreply.github.com>
2025-01-22 16:45:12 +0200

    Update path for tests

--- testsServer/server.js
+++ testsServer/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 const port = 4200;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-app.use(express.static(path.join(__dirname + '/../dist/pivot-angular')));
+app.use(express.static(path.join(__dirname + '/../dist/pivot-angular/browser')));
 
 
 app.get('/', function (req, res) {
