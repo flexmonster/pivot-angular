e4e7518fd45b9881a52b47221b3e468a91ca6c4b
maksymkaD <52499153+maksymkaD@users.noreply.github.com>
2025-01-22 16:21:11 +0200

    Update paths for tests

--- testsServer/server.js
+++ testsServer/server.js
@@ -7,39 +7,39 @@ app.use(express.static(path.join(__dirname + '/../dist/pivot-angular')));
 
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, '/../dist/pivot-angular/index.html'));
+    res.sendFile(path.join(__dirname, '/../dist/pivot-angular/browser/index.html'));
 });
 
 app.get('/with-amcharts', function (req, res) {
-    res.sendFile(path.join(__dirname, '/../dist/pivot-angular/index.html'));
+    res.sendFile(path.join(__dirname, '/../dist/pivot-angular/browser/index.html'));
 });
 
 app.get('/using-api-calls', function (req, res) {
-    res.sendFile(path.join(__dirname, '/../dist/pivot-angular/index.html'));
+    res.sendFile(path.join(__dirname, '/../dist/pivot-angular/browser/index.html'));
 });
 
 app.get('/customizing-grid', function (req, res) {
-    res.sendFile(path.join(__dirname, '/../dist/pivot-angular/index.html'));
+    res.sendFile(path.join(__dirname, '/../dist/pivot-angular/browser/index.html'));
 });
 
 app.get('/customizing-toolbar', function (req, res) {
-    res.sendFile(path.join(__dirname, '/../dist/pivot-angular/index.html'));
+    res.sendFile(path.join(__dirname, '/../dist/pivot-angular/browser/index.html'));
 });
 
 
 app.get('/pivot-table-demo', function (req, res) {
-    res.sendFile(path.join(__dirname, '/../dist/pivot-angular/index.html'));
+    res.sendFile(path.join(__dirname, '/../dist/pivot-angular/browser/index.html'));
 });
 
 
 app.get('/handling-events', function (req, res) {
-    res.sendFile(path.join(__dirname, '/../dist/pivot-angular/index.html'));
+    res.sendFile(path.join(__dirname, '/../dist/pivot-angular/browser/index.html'));
 });
 
 app.get('/with-highcharts', function (req, res) {
-    res.sendFile(path.join(__dirname, '/../dist/pivot-angular/index.html'));
+    res.sendFile(path.join(__dirname, '/../dist/pivot-angular/browser/index.html'));
 });
 
 app.get('/updating-data', function (req, res) {
-    res.sendFile(path.join(__dirname, '/../dist/pivot-angular/index.html'));
+    res.sendFile(path.join(__dirname, '/../dist/pivot-angular/browser/index.html'));
 });
